1e69c43cd9957c8dc58f33a001696e9fbdaf0b68
sheehy <suziesheehy@gmail.com>
2015-06-23 06:16:46 +0100

    added fixed energy COD analysis script
    This probably needs checking/testing…

---
+++ fixedE_COD_20150623.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+import matplotlib
+#matplotlib.rcParams['font.family'] = 'serif'
+#matplotlib.rcParams['legend.fontsize']=12
+from pylab import *
+import numpy
+from analysedata import *
+
+fdirect="2013_11_13_2/"
+nsamples=5000
+degree=10 #This controls how smoothed the data is
+
+###TESTING###
+#fname="double_240.csv"
+#rawdata = readcsv(fdirect+fname, nsamples)
+
+vals = [220, 225, 230, 235, 240, 245, 250, 255, 260, 300, 400]
+
+outfilen="CO_data.dat"
+outf=file(outfilen, 'w')
+
+for v in vals:
+    print "Probe position: ", v
+    fname ="double_"+str(v)+".csv" 
+       
+    rawdata=readcsv(fdirect+fname, nsamples)
+
+    #Create Gaussian smoothed data (uses window of 'degree' & shifts by 'degree' step windowing)
+    smoothdat = smoothListGaussian(rawdata[1], degree)
+
+    ###TESTING###
+    #plot(abs(numpy.array(rawdata[1])[1000:1700]), 'k-')
+    #plot(abs(numpy.array(smoothdat)[1000-degree:1700-degree]), 'r-')
+    #show()
+    #sys.exit()
+
+    x=linspace(0, len(smoothdat), len(smoothdat))
+    data=abs(numpy.array(smoothdat))
+
+    #NOW FIND PEAKS IN SMOOTHED DATA
+    c_keep = peakfinder(data, 2.0)
+
+    # If there are more than 10 peaks, normalise them wrt the 0th (h- injection) peak and write to file
+    if len(c_keep) > 11:
+        ttratio = (data[c_keep[10]]/data[c_keep[0]])
+        #tterror = (data[c_keep[10]]/data[c_keep[0]]).var()
+        outf.write(str(v)+' '+str(ttratio)+'\n')
+        plot(x, data/data[c_keep[0]])
+        plot(x[c_keep], data[c_keep]/data[c_keep[0]], "o", label="max")
+        legend()
+        show()
+    elif len(c_keep) < 11:
+         outf.write(str(v)+' 0.0  0.0 \n')
+
+    ###TESTING###
+    
+
+    #else: print "10 turns not found"
+    
+  
+outf.close()
+
+
